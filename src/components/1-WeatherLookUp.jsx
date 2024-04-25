b738fa7d276192c308fcceccb9713d7307e10767
Hollis-Brown <hollis.brown.cohort231@gmail.com>
2024-04-24 21:10:29 -0700

    Edit
    Change the app title and input placeholder text.

--- src/components/1-WeatherLookUp.jsx
+++ src/components/1-WeatherLookUp.jsx
@@ -1,13 +1,13 @@
 export default function WeatherLookUp() {
   return (
     <>
-      <h1 className="text-center">Weather Woofer Pro</h1>
+      <h1 className="text-center">Weather App Pro</h1>
       <div className="flex justify-center">
         <form className="flex items-center">
           <input
             type="text"
             className="mr-2 rounded-md border border-gray-300 px-3 py-2 focus:outline-none focus:ring focus:ring-blue-500 focus:ring-opacity-50"
-            placeholder="Enter text here"
+            placeholder="Search City"
           />
           <button
             type="submit"
